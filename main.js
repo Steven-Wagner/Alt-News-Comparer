4529d76ec692f6dcf528fad0d1e5cc9ecd5484db
Steven Wagner <steven.glen.wagner@gmail.com>
2019-01-10 18:20:21 -0700

    added js and basic html

---
+++ main.js
@@ -0,0 +1,190 @@
+biasData = {
+    'The New York Times': -5,
+    'The Wall Street Journal': 11,
+    'CNN': -6,
+    'The Huffington Post': -20,
+    'Fox News': 27,
+    'USA Today': 0,
+    'Npr.org': -5,
+    'NBC News': -3,
+    'CBS News': 4,
+    'MSNBC': -19,
+    'breitbart.com': 34,
+    'infowars.com': 44,
+    'politicususa.com': -34,
+    'redstate.com': 29,
+    'wnd.com': 36
+}
+
+function clickSubmit () {
+    $('form').submit(event => {
+        event.preventDefault();
+        let searchTerm = $('#search').val();
+        fetchData (searchTerm);
+    })
+}
+
+function fetchData (searchTerm) {
+    console.log('test');
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://api-hoaxy.p.mashape.com/articles?sort_by=relevant&use_lucene_syntax=true&query=${searchTerm}`,
+        "method": "GET",
+        "headers": {
+          "Accept": "application/json",
+          "X-Mashape-Key": "TcWIAYm5YCmsh3fBLAJ3Rz05FOxHp1sBjoDjsnV01p5E4gGfIp",
+          "cache-control": "no-cache",
+          "Postman-Token": "c364eacd-3d1c-4aa3-aa27-b93cf02f7b9a"
+        }
+      }
+      
+      $.ajax(settings).done(function (response) {
+        let altNewsData = response;
+        displayAltNews(altNewsData);
+        displayFactCheckNews(altNewsData);
+
+        lastMonthdate = getDate();
+        
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": `https://newsapi.org/v2/everything?apiKey=ba81d44e6d054cc9b78df51ce86a46f0&domains=wsj.com,nytimes.com,cnn.com,huffingtonpost.com,foxnews.com,usatoday.com,%20npr.org,%20nbcnews.com,%20cbsnews.com,%20abcnews.com,%20newsweek.com%20&q=${searchTerm}&from=${lastMonthdate}&sortBy=publishedAt`,
+            "method": "GET",
+          }
+          
+          $.ajax(settings).done(function (newsResponse) {
+            let mainMediaData = newsResponse;
+            displayMainNews(mainMediaData);
+          });
+      });
+}
+
+function getDate () {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth()+1;
+    let year = date.getFullYear();
+    if (month-1 === 0) {
+        month = 12;
+        year--;
+    }
+    else {
+        month--;
+    }
+    currentdate = `${year}-${month}-${day}`;
+}
+
+function displayFactCheckNews (altNewsData) {
+    factChecks = [];
+    altData = altNewsData['articles'];
+    for (let i=0; i<altData.length; i++){
+        if (factChecks.length<=10){
+            if (altData[i]['site_type'] === 'fact_checking') {
+                factChecks.push(altData[i]);
+            }
+        }
+        else {
+            break;
+        }
+    }
+    let factCheckHTML = HTMLAltFactNews (factChecks);
+    $('.fact-check-results').html(factCheckHTML);
+}
+
+
+function displayAltNews(altNewsData) {
+    altData = altNewsData['articles'];
+    altNews = [];
+    console.log(altData);
+    let max=10;
+    for (let i=0; i<max; i++){
+        console.log(altData[i]);
+        if (altData[i]['site_type'] === 'fact_checking'){
+            max += 1;
+        }
+        else {
+            altNews.push(altData[i]);
+        }
+    }
+    let altHTML = HTMLAltFactNews (altNews);
+    $('.alt-results').html(altHTML);
+}
+
+function HTMLAltFactNews (altNews) {
+    let altArticlesHTML = '';
+    for (let i=0; i<altNews.length; i++) {
+        let title = altNews[i]['title'];
+        let name = altNews[i]['domain'];
+        let biasDiscription = biasScore(name);
+        let url = altNews[i]['canonical_url'];
+        altArticlesHTML += `<li><h3>${title}</h3><p>${name}</p><p>${biasDiscription}</p><a target="_blank" href="${url}">${url}</a>`;
+    }
+    return altArticlesHTML;
+    
+}
+
+function displayMainNews(mainMediaData) {
+    let articles = []
+    let articlesData = mainMediaData['articles'];
+    for (let i=0; i<10; i++) {
+        articles.push(articlesData[i])
+    }
+    let mainArticlesHTML = HTMLMainStreamNews (articles);
+    $('.main-results').html(mainArticlesHTML);
+}
+
+function HTMLMainStreamNews (articles) {
+    articleHTML = '';
+    for (let i=0; i<articles.length; i++){
+        if (typeof articles[0] == 'undefined') {
+            return `<li>No Results Found</li>`;
+        }
+        else if (typeof articles[i] == 'undefined'){
+            return articleHTML;
+            }
+        else {
+            let title = articles[i]['title'];
+            let source = articles[i]['source']['name'];
+            let biasDescription = biasScore(source);
+            let url = articles[i]['url'];
+            let description = articles[i]['description'];
+            articleHTML += `<li><h3>${title}</h3><p>${source}</p><p>${biasDescription}</p><p>${description}</p><a target="_blank" href="${url}">${url}<a>`;
+        }
+    }
+
+    return articleHTML;
+}
+
+function biasScore(source) {
+    let biasDescriptor = '';
+    Object.keys(biasData).forEach(bias => {
+        if (source === bias) {
+            biasNum = biasData[bias];
+            if (biasNum>-6 & biasNum<6) {
+                biasDescriptor = "Bias: Center"
+            }
+            else if (biasNum>-11 & biasNum<-5) {
+                biasDescriptor = "Bias: Leans Left";
+            }
+            else if (biasNum>-21 & biasNum<-10) {
+                biasDescriptor = "Bias: Partisan Left";
+            }
+            else if (biasNum>5 & biasNum<11) {
+                biasDescriptor = "Bias: Leans Right";
+            }
+            else if (biasNum>10 & biasNum<21) {
+                biasDescriptor = "Bias: Partisan Right";
+            }
+            else if (biasNum<-20) {
+                biasDescriptor = "Bias: Extream Left";
+            }
+            else if (biasNum>20) {
+                biasDescriptor = "Bias: Extream Right";
+            }
+        }
+    })
+    return biasDescriptor;
+}
+
+$(clickSubmit);
